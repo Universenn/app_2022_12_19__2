1b2ffa4e70c9c54f06816eabb76554767133441e
Universenn <dnwndhks98@gmail.com>
2022-12-19 10:56:06 +0900

    세팅

---
+++ src/main/java/com/ll/app_2022_12_19__2/App202212192Application.java
@@ -0,0 +1,13 @@
+package com.ll.app_2022_12_19__2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App202212192Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App202212192Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/app_2022_12_19__2/App202212192ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.app_2022_12_19__2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App202212192ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
